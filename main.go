cdfd1df24c8ed55562db2174f606263fc763d39f
wdevid <821017115@qq.com>
2017-09-07 16:03:26 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/astaxie/beego"
+	"samples/mimi/controllers"
+)
+
+func main() {
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/v1/shorten", &controllers.ShortController{})
+	beego.Router("/v1/expand", &controllers.ExpandController{})
+	beego.Run()
+}
